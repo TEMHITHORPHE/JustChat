5b73eb1ff5468132c934a6950b065ce8dacac2d7
TEMHITHORPHE <Tehmhithorphe@gmail.com>
2023-07-17 12:53:57 +0100

    minor refactor

--- src/main.js
+++ src/main.js
@@ -8,10 +8,11 @@ import 'dotenv/config';
 const app = express();
 
 app.get('/', function (_, res) {
-	res.send('Just Chat AI Assistant!! ');
+	res.send('JustChat AI Assistant!! ');
 })
 
 app.use("/bot/webhook", WebhookRoutes);
 
 app.listen(process.env.EXPRESS_SERVER_LISTENING_PORT || 3000);
+
 console.log(`Listening on: http://localhost:${process.env.EXPRESS_SERVER_LISTENING_PORT}`);
