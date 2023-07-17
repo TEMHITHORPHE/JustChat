0eb26f47c9c47413e3734cfade5d7479f47b1e29
TEMHITHORPHE <Tehmhithorphe@gmail.com>
2023-07-17 12:53:29 +0100

    Added: OnMessage implementation

--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -1,5 +1,5 @@
 
-import { Router } from "express";
+import { Router, json } from "express";
 import { WHATSAPP } from "./whatsapp.js";
 
 
@@ -8,32 +8,48 @@ export const WebhookRoutes = Router();
 
 
 WebhookRoutes.use((req, _, next) => {
-	console.log("WEBHOOK HANDLER: ", req.headers, req.params, req.query, "\n:::WEBHOOK HANDLER:::");
+	console.log("WEBHOOK HANDLER: ", req.headers, "\n:::WEBHOOK HANDLER:::\n");
 	next();
 });
 
 
+// Route is called on POST request to "/bot/webhook/" path
 WebhookRoutes.post("/", async (req, res) => {
 
-	// Assuming post is called on a POST request to your server
-	try {
+	console.log("\n[POST]::[WEBHOOK - ROUTE]:");
 
-		// The handlers work with any middleware, as long as you pass the correct data
-		const status = await WHATSAPP.post(JSON.parse(req.data), req.data, req.headers["x-hub-signature-256"]);
-		console.log("[WEBHOOK_POST STATUS]: ", status);
-		res.sendStatus(status);
+	const chunks = [];
+	req.on("data", (chunk) => chunks.push(chunk));
+	req.on("end", async () => {
 
-	} catch (error) {
+		const body = Buffer.concat(chunks).toString();
+		const parsedBody = JSON.parse(body); 
+		console.log("\n\n:::::::::::::: DATA :::::::::::::::::\n", parsedBody, "  changes:", parsedBody.entry[0].changes[0], "\n[POST]::[WEBHOOK - ROUTE]:");
 
-		console.log("[WEBHOOK_POST ERROR]: ", error);
-		res.sendStatus(error);
+		try {
 
-	}
-	return;
+			// The handlers work with any middleware, as long as we pass the correct data
+			const status = await WHATSAPP.post(parsedBody, body, req.headers["x-hub-signature-256"]);
+			console.log("[WEBHOOK_POST STATUS]: ", status);
+			res.sendStatus(status);
+
+		} catch (error) {
+
+			console.log("[WEBHOOK_POST ERROR]: ", error);
+			res.sendStatus(error);
+
+		}
+
+		res.end();
+
+	});
+
 });
 
 
-WebhookRoutes.get("/verify", (req, res) => {
+WebhookRoutes.get("/", (req, res) => {
+	console.log(req.query, req.params, req.body, "[GET]::[WEBHOOK - ROUTE]:");
+
 	try {
 		const challenge = WHATSAPP.get(req.query);
 		res.send(challenge);
